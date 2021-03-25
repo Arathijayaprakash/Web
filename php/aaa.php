77cd504be6f0b5523af6dc25817b198a3b7d8f56
Arathijayaprakash <arathiparambil@gmail.com>
2021-03-25 15:03:59 +0530

    php

---
+++ php/aaa.php
@@ -0,0 +1,30 @@
+<html>
+<head>
+<title>insert data in form</title>
+</head>
+<body>
+<form action="getdata.php" method="POST">
+name:<input type="text" name="txtname">
+<br><br>
+rollno:
+<input type="text" name="txtr_no">
+<br><br>
+address:
+<textarea name="add" type="textarea">
+</textarea>
+<br>
+<br>
+contact number:
+<input type="text" name="txtc_no">
+<br>
+<br>
+email id:
+<input type="text" name="txteld">
+<br>
+<br>
+<input type="submit" name="insert" value="save">
+<input type="reset" value="cancel">
+</form>
+</body>
+</html>
+
---
+++ php/getdata.php
@@ -0,0 +1,16 @@
+<html> 
+<head>
+<title>
+get data from another page
+</title>
+</head>
+<body>
+<?php
+echo"name:".$_POST["txtname"]."</br>";
+echo"rollno:".$_POST["txtr_no"]."</br>";
+echo"address:".$_POST["add"]."</br>";
+echo"contact number:".$_POST["txtc_no"]."</br>";
+echo"email id:".$_POST["txteld"]."</br>";
+?>
+</body>
+</html>
